6347ec89fca60f03110b47dff0e2f598dc00f5bc
Roger Peter Kohler <ropeko@yahoo.de>
2019-01-02 20:16:59 +0100

    Added Geolocation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    CommonModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    Geolocation
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Geolocation, Coordinates, Geoposition } from '@ionic-native/geolocation/ngx';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: 'home.page.html'
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
+
+  position: Coordinates;
+
+  private subscription: Subscription;
+
+  constructor(private geolocation: Geolocation) {}
+
+  ngOnInit(): void {
+    this.geolocation.getCurrentPosition().then(position => this.position = position.coords);
+    this.subscription = this.geolocation.watchPosition().subscribe(position => {
+      console.log('old position:', this.position);
+      this.position = position.coords;
+      console.log('new position:', this.position);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
 }
